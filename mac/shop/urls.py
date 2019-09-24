0dfb48eec37b23700687a4b6b2d0542a84af126f
savalitaliya11 <savalitaliya11@gmail.com>
2019-09-24 15:57:14 +0530

    adding more feature

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -1,7 +1,14 @@
+# shop/urls.py
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='shopname'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact us'),
+    path('tracker', views.tracker, name='tracker'),
+    path('search', views.search, name='search'),
+    path('productview', views.productview, name='productview'),
+    path('checkout', views.checkout, name='checkout'),
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,6 +1,31 @@
+# shop/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("Hey there,this is about")
+
+
+def contact(request):
+    return HttpResponse("Hey there,this is contact")
+
+
+def tracker(request):
+    return HttpResponse("Hey there,this is tracker")
+
+
+def search(request):
+    return HttpResponse("Hey there,this is search")
+
+
+def productview(request):
+    return HttpResponse("Hey there,this is p-view")
+
+
+def checkout(request):
+    return HttpResponse("Hey there,this is checkout")
