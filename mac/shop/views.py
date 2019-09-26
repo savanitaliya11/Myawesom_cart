e86716ccf6e6b6719c87e2973dec3d9d2648986f
savalitaliya11 <savalitaliya11@gmail.com>
2019-09-26 16:51:38 +0530

    showing product logic

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,10 +1,20 @@
 # shop/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 
 def index(request):
-    return render(request, 'shop/index.html')
+    p = Product.objects.all()
+    print(p)
+    n = len(p)
+    nSlides = n // 4 + ceil((n / 4) - (n // 4))
+    # consider n=4 then,n//4 = 1 + ceil((n/4 = 1)-(n//4 = 1)) = 0 then no. of slides is 1
+    # consider n=5 n//5 = 1.25 consider(1) + ceil((1.25)-(1)) = 1.25 consider (2 because 1.25
+    # is o/p of ceil)then oyr no. of slides is 2
+    dict = {'no.of slides': nSlides, 'range': (1, nSlides), 'product': p}
+    return render(request, 'shop/index.html', dict)
 
 
 def about(request):
